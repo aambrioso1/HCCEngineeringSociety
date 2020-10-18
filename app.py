b2c746da3c194fb632367fab8d94a62a30b86b10
aambrioso <aambrioso1@gmail.com>
2020-10-18 16:17:05 -0400

    Add galaxy_stuff.

--- app.py
+++ app.py
@@ -14,16 +14,19 @@ def page_not_found(e):
 def internal_server_error(e):
     return render_template('500.html'), 500
 
-
 @app.route('/')
 def index():
     return render_template('index.html')
-
 
 @app.route('/user/<name>')
 def user(name):
 	    return render_template('user.html', name=name)
 
+@app.route('/galaxy_stuff')
+def galaxy_stuff():
+	# return render_tamplate('')
+	return render_template('/galaxy_stuff/galaxy_index.html')
+
 
 if __name__ == "__main__":
-    app.run(debug=True,port=5000)
+    app.run(debug=True, port=5000)
